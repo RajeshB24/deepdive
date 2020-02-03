fac80a1054993ee17f222bbce957728f992573c6
rajeshb24 <rajeshbalakrishnan24@gmail.com>
2020-02-03 17:45:54 +0530

    deeptree predict corrections

--- R/predict.deeptree.R
+++ R/predict.deeptree.R
@@ -1,5 +1,6 @@
 
 
+
 #' Predict Function for Deeptree
 #'
 #' @param object deeptree model object
@@ -18,76 +19,81 @@
 #' @importFrom graphics barplot
 #' @importFrom stats formula predict runif
 
-predict.deeptree<-function(object,newData,treeLeaves=NA,stackPred=NA,...){
+predict.deeptree <-
+  function(object,
+           newData,
+           treeLeaves = NA,
+           stackPred = NA,
+           ...) {
+    if (!inherits(object, "deeptree"))
+      stop("Not a legitimate \"deeptree\" object")
+
+    useStackPred = object$useStackPred
+
+    if (length(useStackPred) < length(object$treeLeavesGroup)) {
+      useStackPred <- rep(useStackPred, length(object$treeLeavesGroup))
+    }
+
+    if (sum(useStackPred) > 0) {
+      if (is.na(sum(stackPred))) {
+        print('stackPred is required')
+
+      }
+    }
+    treeLeavesGroup <- object[['treeLeavesGroup']]
+    modelGroup <- object[['modelGroup']]
+    xCols = object[["xColList"]]
+
+    if (object[["preBuiltTree"]]) {
+      if (is.na(treeLeaves)) {
+        print("missing treeLeaves attribute")
 
 
+      }
+    } else{
+      treeLeaves = rpart.predict.leaves(object[["treeMod"]], newData)
 
-  if (!inherits(object, "deeptree")) stop("Not a legitimate \"deeptree\" object")
-
-  useStackPred=object$useStackPred
-
-  if(length(useStackPred)<length(object$treeLeavesGroup)){
-    useStackPred<-rep(useStackPred,length(object$treeLeavesGroup))
-  }
-
-  if(sum(useStackPred)>0){
-    if(is.na(sum(stackPred))){
-      print('stackPred is required')
 
     }
+
+
+    newDataSplit <- lapply(treeLeavesGroup, function(tp) {
+      newData[treeLeaves == tp, ]
+    })
+
+    if (sum(useStackPred) > 0) {
+      stackPred <- lapply(treeLeavesGroup, function(tp) {
+        stackPred[treeLeaves == tp, ]
+      })
+    }
+
+
+
+    predlist <-  lapply(1:length(modelGroup)
+                        , function(mg) {
+                          if (!useStackPred[[mg]]) {
+                            predict.deepnet(modelGroup[[mg]], newDataSplit[[mg]][, xCols[[mg]]])
+                          } else{
+                            stackPred[[mg]]
+                          }
+
+                        })
+
+    predlist <- lapply(1:length(predlist), function(pl) {
+      data.frame(cbind(predlist[[pl]], rowname = as.numeric(row.names(newDataSplit[[pl]]))))
+
+    })
+
+
+    ypred <- data.frame(rbindlist(predlist, use.names = T,
+                                  fill = T,
+    ))
+
+
+    ypred[is.na(ypred)]=0
+    ypred <- ypred[order(ypred$rowname), ]
+    # names(ypred)[1] <- c('pred_y')
+
+
+    return(data.frame(pred_y = ypred))
   }
-  treeLeavesGroup<-object[['treeLeavesGroup']]
-  modelGroup<-object[['modelGroup']]
-  xCols=object[["xColList"]]
-
-if(object[["preBuiltTree"]]){
-
-  if(is.na(treeLeaves)){
-    print("missing treeLeaves attribute")
-
-
-  }}else{
-
-    treeLeaves=rpart.predict.leaves(object[["treeMod"]],newData)
-
-
-  }
-
-
-  newDataSplit<- lapply(treeLeavesGroup, function(tp){
-    newData[treeLeaves==tp,]
-  })
-
-  if(sum(useStackPred)>0){
-  stackPred<-lapply(treeLeavesGroup, function(tp){
-    stackPred[treeLeaves==tp,]
-  })
-}
-
-
-
-  predlist<-  lapply(1:length(modelGroup)
-                     , function(mg){
-
-                       if(!useStackPred[[mg]]){
-                       predict.deepnet(modelGroup[[mg]],newDataSplit[[mg]][,xCols[[mg]]])
-                       }else{
-                         stackPred[[mg]]
-                       }
-
-                     })
-
-  predlist<-lapply(1:length(predlist), function(pl){
-    data.frame( cbind(predlist[[pl]],rowname= as.numeric(row.names(newDataSplit[[pl]]))))
-
-  })
-
-
-  ypred<-data.frame(rbindlist(predlist,use.names = F))
-
-  ypred<-ypred[order(ypred$rowname),]
-  names(ypred)[1]<-c('pred_y')
-
-
-  return(data.frame(pred_y=ypred[,'pred_y']))
-}
