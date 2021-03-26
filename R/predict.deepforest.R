f9c5778ecce9dc310082ce0acc1ad1e2946b76ad
RajeshB24 <rajeshbalakrishnan24@gmail.com>
2021-03-26 21:01:13 +0530

    predict deepforest fix for error on regression problems

--- R/predict.deepforest.R
+++ R/predict.deepforest.R
@@ -14,81 +14,59 @@
 #' @importFrom graphics barplot
 #' @importFrom stats formula predict runif
 
-predict.deepforest<-function(object,
-                             newData,...){
-
-
-  if (!inherits(object, "deepforest")) stop("Not a legitimate \"deepforest\" object")
-
-  varCut=object$varcut
-  modelGroup=object$chosenModels
-  treeAugment=object$treeAugment
-
-  o<-1
-  predlist<-  lapply(1:length(modelGroup)
-                     , function(o){
-
-                       if(treeAugment==F){
-
-
-                         xFit<-data.frame(newData[,varCut[[o]]])
-                         names(xFit)<-varCut[[o]]
-
-
-                       deepnetPred<-predict.deepnet(modelGroup[[o]],xFit)
+predict.deepforest<-function (object, newData, ...)
+{
+  if (!inherits(object, "deepforest"))
+    stop("Not a legitimate \"deepforest\" object")
+  varCut = object$varcut
+  modelGroup = object$chosenModels
+  treeAugment = object$treeAugment
 
 
 
+  predlist <- lapply(1:length(modelGroup), function(o) {
+    if (treeAugment == F) {
+      xFit <- data.frame(newData[, varCut[[o]]])
+      names(xFit) <- varCut[[o]]
+      deepnetPred <- predict.deepnet(modelGroup[[o]], xFit)
 
-                       deepnetPred<-deepnetPred[,names(deepnetPred)!="ypred"]
-                       for( i in names(deepnetPred)){
+      if(deepforestMod$modelType!='regress'){
+        deepnetPred <- deepnetPred[, names(deepnetPred) != "ypred"]
+      }
 
-                         deepnetPred[,i]   <-as.numeric(deepnetPred[,i] )
+      for (i in names(deepnetPred)) {
+        deepnetPred[, i] <- as.numeric(deepnetPred[,i])
+      }
+      deepnetPred
+    }
+    else {
+      xFit <- data.frame(newData[, varCut[[o]]])
+      names(xFit) <- varCut[[o]]
+      augmentPred <- predict.deeptree(modelGroup[[o]],
+                                      newData = xFit)
+      if (object$modelType == "multiClass") {
+        augmentPred <- augmentPred[, names(augmentPred) !=
+                                     "ypred"]
+        for (i in names(augmentPred)) {
+          augmentPred[, i] <- as.numeric(augmentPred[,
+                                                     i])
+        }
+      }
+      augmentPred
+    }
+  })
+  networkCount <- length(modelGroup)
+  predarray <- array(unlist(predlist), c(nrow(newData), ncol(predlist[[1]]),
+                                         networkCount))
 
-                       }
-                    deepnetPred
-
-                         }else{
-
-                         xFit<-data.frame(newData[,varCut[[o]]])
-                         names(xFit)<-varCut[[o]]
-
-                         augmentPred<- predict.deeptree(modelGroup[[o]],
-                           newData =xFit)
-
-                         if(object$modelType=="multiClass"){
-                         augmentPred<-augmentPred[,names(augmentPred)!="ypred"]
-
-
-                        for( i in names(augmentPred)){
-
-                          augmentPred[,i]   <-as.numeric(augmentPred[,i] )
-
-                        }}
-                       augmentPred
-
-
-                       }
-
-                     })
-
-
-  networkCount<-length(modelGroup)
-
-predarray<-array(unlist(predlist),c(nrow(newData),ncol(predlist[[1]]),networkCount))
-
-predDf<-data.frame(apply(predarray, 1:2, mean))
-names(predDf)<-colnames(predlist[[1]])
-
-
-if(object$modelType=="multiClass"){
-  predDf$ypred<-stringr::str_remove_all( names(predDf),"y_")[max.col(predDf)]
-  names(predDf)=stringr::str_remove_all(names(predDf),"y_")
-}
-
-
-
+  predDf <- data.frame(apply(predarray, 1:2, mean))
+  names(predDf) <- colnames(predlist[[1]])
+  if (object$modelType == "multiClass") {
+    predDf$ypred <- stringr::str_remove_all(names(predDf),
+                                            "y_")[max.col(predDf)]
+    names(predDf) = stringr::str_remove_all(names(predDf),
+                                            "y_")
+  }
   return(predDf)
 }
-
 
