35672635d9ac56665b8ca3695d57442aaaf83960
Rajesh B <rajeshbalakrishnan24@gmail.com>
2021-07-10 03:25:35 +0530

    Update predict.deeptree.R
    Deeptree mismatched prediction fix

--- R/predict.deeptree.R
+++ R/predict.deeptree.R
@@ -20,132 +20,113 @@
 #' @importFrom stats formula predict runif
 #'
 
-predict.deeptree <-
-  function (object, newData, treeLeaves = NA, stackPred = NA, ...)
-  {
-
-    row.names(newData)<-1:nrow(newData)
-    if (!inherits(object, "deeptree"))
-      stop("Not a legitimate \"deeptree\" object")
-    useStackPred = object$useStackPred
-
-
-    if (length(useStackPred) < length(object$treeLeavesGroup)) {
-      useStackPred <- rep(useStackPred, length(object$treeLeavesGroup))
+predict.deeptree= function (object, newData, treeLeaves = NA, stackPred = NA, ...)
+{
+  row.names(newData) <- 1:nrow(newData)
+  if (!inherits(object, "deeptree"))
+    stop("Not a legitimate \"deeptree\" object")
+  useStackPred = object$useStackPred
+  if (length(useStackPred) < length(object$treeLeavesGroup)) {
+    useStackPred <- rep(useStackPred, length(object$treeLeavesGroup))
+  }
+  if (sum(useStackPred) > 0) {
+    if (is.na(sum(stackPred))) {
+      warning("stackPred is required")
     }
-    if (sum(useStackPred) > 0) {
-      if (is.na(sum(stackPred))) {
-        warning("stackPred is required")
-      }
+  }
+  treeLeavesGroup <- object[["treeLeavesGroup"]]
+  modelGroup <- object[["modelGroup"]]
+  xCols = object[["xColList"]]
+  if (object[["preBuiltTree"]]) {
+    if (is.na(treeLeaves)) {
+      warning("missing treeLeaves attribute")
     }
-
-    treeLeavesGroup <- object[["treeLeavesGroup"]]
-
-    modelGroup <- object[["modelGroup"]]
-
-
-    xCols = object[["xColList"]]
-
-    if (object[["preBuiltTree"]]) {
-      if (is.na(treeLeaves)) {
-        warning("missing treeLeaves attribute")
-      }
-    }
-    else {
-      treeLeaves = rpart.predict.leaves(object[["treeMod"]],
-                                        newData)
-
-
-    }
-
-    newDataSplit <- lapply(treeLeavesGroup[treeLeavesGroup %in% unique(treeLeaves)],
-                           function(tp) {
-                             sdata <- data.frame(newData[treeLeaves == tp, ])
-                             names(sdata) <- names(newData)
-                             sdata
-                           })
-
-    if (sum(useStackPred) > 0) {
-      stackPred <- lapply(treeLeavesGroup, function(tp) {
-        stackPred[treeLeaves == tp, ]
-      })
-    }
+  }else {
+    treeLeaves = rpart.predict.leaves(object[["treeMod"]],
+                                      newData)
+  }
+  newDataSplit <- lapply(treeLeavesGroup[treeLeavesGroup %in%
+                                           unique(treeLeaves)], function(tp) {
+                                             sdata <- data.frame(newData[treeLeaves == tp, ])
+                                             names(sdata) <- names(newData)
+                                             sdata
+                                           })
+  if (sum(useStackPred) > 0) {
+    stackPred <- lapply(treeLeavesGroup, function(tp) {
+      stackPred[treeLeaves == tp, ]
+    })
+  }
 
 
-    predlist <- lapply(1:length(unique(treeLeaves)), function(mg) {
-      if (!useStackPred[[mg]]) {
-        if (class(modelGroup[[mg]]) == "deepnet") {
-          if (length(xCols[[mg]]) == 1) {
-            predict.deepnet(modelGroup[[mg]],
-                            data.frame(newDataSplit[[mg]][,
-                                                          xCols[[mg]]]))
+  modelGroup = modelGroup[ treeLeavesGroup %in% unique(treeLeaves)]
 
-          }
-          else {
-            predict.deepnet(modelGroup[[mg]], newDataSplit[[mg]][,
-                                                                 xCols[[mg]]])
-          }
+  predlist <- lapply(1:length(unique(treeLeaves)), function(mg) {
+    if (!useStackPred[[mg]]) {
+      if (class(modelGroup[[mg]]) == "deepnet") {
+        if (length(xCols[[mg]]) == 1) {
+          predict.deepnet(modelGroup[[mg]], data.frame(newDataSplit[[mg]][,
+                                                                          xCols[[mg]]]))
         }
         else {
-          ypred = data.frame(ypred = rep(modelGroup[[mg]],
-                                         nrow(newDataSplit[[mg]])))
-          ypred
+          predict.deepnet(modelGroup[[mg]], newDataSplit[[mg]][,
+                                                               xCols[[mg]]])
         }
       }
       else {
-        stackPred[[mg]]
-      }
-    })
-
-    predlist <- lapply(1:length(predlist), function(pl) {
-      data.frame(cbind(predlist[[pl]], rowname = as.numeric(row.names(newDataSplit[[pl]]))))
-    })
-
-    ypred <- data.frame(rbindlist(predlist, use.names = T, fill = T
-    ))
-
-    ypred[is.na(ypred)] = 0
-
-    ypred <- ypred[order(as.numeric(row.names(ypred))), ]
-
-
-
-    if (object$modelType == "regress") {
-      names(ypred)[1] <- "ypred"
-    }
-    else if (object$modelType == "multiClass") {
-      ypred$ypred <- stringr::str_remove_all(ypred$ypred, "y..s.._")
-      names(ypred) <- stringr::str_remove_all(names(ypred),
-                                              "y..s.._")
-      if (length(unique(ypred$ypred)) > sum(!names(ypred) %in%
-                                            c("rowname", "ypred"))) {
-        for (coli in unique(ypred$ypred)[!unique(ypred$ypred) %in%
-                                         names(ypred)]) {
-          ypred[, coli] <- ifelse(ypred$ypred == coli,
-                                  1, 0)
-        }
-      }
-      colnames <- names(ypred)[!names(ypred) %in% "rowname"]
-      ypred <- data.frame(ypred[, !names(ypred) %in% "rowname"])
-      names(ypred) <- colnames
-      ProbCols <- names(ypred)[!names(ypred) %in% "ypred"]
-      ypred$ProbSum <- rowSums(ypred[, ProbCols])
-      for (icol in 1:length(ProbCols)) {
-        ypred[, icol] <- ifelse(ypred$ProbSum == 0 & as.character(ypred[,
-                                                                        icol]) == as.character(ypred$ypred), 1, as.character(ypred[,
-                                                                                                                                   icol]))
+        ypred = data.frame(ypred = rep(modelGroup[[mg]],
+                                       nrow(newDataSplit[[mg]])))
+        ypred
       }
     }
-    if(object$modelType=='regress'){
-      ypred<-ypred['ypred']
+    else {
+      stackPred[[mg]]
+    }
+  })
+  predlist <- lapply(1:length(predlist), function(pl) {
+    data.frame(cbind(predlist[[pl]], rowname = as.numeric(row.names(newDataSplit[[pl]]))))
+  })
+  ypred <- data.frame(rbindlist(predlist, use.names = T, fill = T))
+  ypred[is.na(ypred)] = 0
+  ypred <- ypred[order(ypred$rowname), ]
 
-    }else{
+  row.names(ypred) <- 1:nrow(ypred)
 
-      finColNames <- names(ypred)
-      finColNames <- finColNames[!finColNames %in% c("rowname",
-                                                     "ProbSum")]
-      ypred = ypred[, finColNames]
-      names(ypred) <- finColNames}
-
-    return(data.frame(ypred))
+  if (object$modelType == "regress") {
+    names(ypred)[1] <- "ypred"
   }
+  else if (object$modelType == "multiClass") {
+    ypred$ypred <- stringr::str_remove_all(ypred$ypred, "y..s.._")
+    names(ypred) <- stringr::str_remove_all(names(ypred),
+                                            "y..s.._")
+    if (length(unique(ypred$ypred)) > sum(!names(ypred) %in%
+                                          c("rowname", "ypred"))) {
+      for (coli in unique(ypred$ypred)[!unique(ypred$ypred) %in%
+                                       names(ypred)]) {
+        ypred[, coli] <- ifelse(ypred$ypred == coli,
+                                1, 0)
+      }
+    }
+    colnames <- names(ypred)[!names(ypred) %in% "rowname"]
+    ypred <- data.frame(ypred[, !names(ypred) %in% "rowname"])
+    names(ypred) <- colnames
+    ProbCols <- names(ypred)[!names(ypred) %in% "ypred"]
+    ypred$ProbSum <- rowSums(ypred[, ProbCols])
+    for (icol in 1:length(ProbCols)) {
+      ypred[, icol] <- ifelse(ypred$ProbSum == 0 & as.character(ypred[,
+                                                                      icol]) == as.character(ypred$ypred), 1, as.character(ypred[,
+                                                                                                                                 icol]))
+    }
+  }
+  if (object$modelType == "regress") {
+    ypred <- ypred["ypred"]
+  }
+  else {
+    finColNames <- names(ypred)
+    finColNames <- finColNames[!finColNames %in% c("rowname",
+                                                   "ProbSum")]
+    ypred = ypred[, finColNames]
+    names(ypred) <- finColNames
+  }
+  return(data.frame(ypred))
+}
+
